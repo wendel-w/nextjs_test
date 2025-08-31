4d6abe4c04584113e061df3c66eef277046646da
vendel <vendel.tout@yandex.com>
2025-08-31 14:50:50 +0300

    first

---
+++ _next/static/chunks/99f3f72d203d09b5.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/921d0879b7aa6a7c.js",
+  "static/chunks/turbopack-8efe85b8d10a0ce4.js"
+])
---
+++ _next/static/chunks/c70de83cb71d021a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/921d0879b7aa6a7c.js",
+  "static/chunks/turbopack-75f04b54c4b2fd1d.js"
+])
